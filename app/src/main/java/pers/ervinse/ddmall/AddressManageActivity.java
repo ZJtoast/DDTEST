377d15b87061b4a639d4e86fda85aeedf6128a8a
ManYan <871799193@qq.com>
2023-07-04 11:19:40 +0800

    更新购物车结算页面，更新药物详情页面

--- app/src/main/java/pers/ervinse/ddmall/AddressManageActivity.java
+++ app/src/main/java/pers/ervinse/ddmall/AddressManageActivity.java
@@ -1,20 +1,18 @@
 package pers.ervinse.ddmall;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class AddressManageActivity extends AppCompatActivity {
-
+public class AddressManageActivity extends Activity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
     private Context mContext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment);
-        mContext = this;
 
     }
 }
---
+++ app/src/main/java/pers/ervinse/ddmall/CommentInfo.java
@@ -0,0 +1,66 @@
+package pers.ervinse.ddmall;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CommentInfo extends AppCompatActivity {
+
+    private Context mContext;
+    private ListView drug_lv;
+    protected void onRestart() {
+        super.onRestart();
+        setAdapter();
+    }
+
+    public List<? extends Map<String, ?>> getlist() {
+        ArrayList<HashMap<String, Object>> address = new ArrayList<>();
+        //   String responseJson = null;
+        //  try {
+        //      responseJson = OkhttpUtils.doGet(url + "/暂定");
+        //  } catch (IOException e) {
+        //      Log.i(TAG, "请求药品评论错误");
+        //  }
+        // Log.i(TAG, "登录请求响应json:" + responseJson);
+        //  UserResponse<Boolean> response = gson.fromJson(responseJson, UserResponse.class);
+        //  Log.i(TAG, "登录请求响应解析数据:" + responseJson);
+        return address;
+    }
+
+    public void setAdapter() {
+        SimpleAdapter comment = new SimpleAdapter(
+                mContext, getlist(), R.layout.item_medicine,
+                new String[]{"商家", "商品名称", "商品图片"},
+                new int[]{R.id.merchant_image, R.id.drug_image, R.id.comment_drug_button});
+        ListView sv = findViewById(R.id.drug_lv);
+        sv.setAdapter(comment);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+        mContext = this;
+//        List<Map<String, Object>> list = new ArrayList<>();
+//        for (int i = 0; i < imgs.length; i++) {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("icon", imgs[i]);
+//            map.put("name", tits[i]);
+//            map.put("text", strs[i]);
+//            list.add(map);
+//        }
+
+    }
+
+
+}
